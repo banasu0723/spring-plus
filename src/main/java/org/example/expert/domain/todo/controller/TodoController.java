3d95f6d89a9155b4f332f3fbf520bd58dc13cd06
banasu0723 <banasu0723@gmail.com>
2024-10-10 02:02:37 +0900

    [1-5] JPA의 이해

--- src/main/java/org/example/expert/domain/todo/controller/TodoController.java
+++ src/main/java/org/example/expert/domain/todo/controller/TodoController.java
@@ -9,8 +9,12 @@ import org.example.expert.domain.todo.dto.response.TodoResponse;
 import org.example.expert.domain.todo.dto.response.TodoSaveResponse;
 import org.example.expert.domain.todo.service.TodoService;
 import org.springframework.data.domain.Page;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,13 +33,18 @@ public class TodoController {
     @GetMapping("/todos")
     public ResponseEntity<Page<TodoResponse>> getTodos(
             @RequestParam(defaultValue = "1") int page,
-            @RequestParam(defaultValue = "10") int size
-    ) {
-        return ResponseEntity.ok(todoService.getTodos(page, size));
-    }
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam (required = false) String weather,
+            @RequestParam (required = false) @DateTimeFormat(pattern = "yyyy-mm-dd") LocalDate startDate,
+            @RequestParam (required = false) @DateTimeFormat(pattern = "yyyy-mm-dd")LocalDate endDate
+            ) {
+        Page<TodoResponse> todos = todoService.getTodos(page, size, weather, startDate, endDate);
+        return ResponseEntity.ok(todoService.getTodos(page, size,weather,startDate,endDate));
+        }
 
     @GetMapping("/todos/{todoId}")
     public ResponseEntity<TodoResponse> getTodo(@PathVariable long todoId) {
         return ResponseEntity.ok(todoService.getTodo(todoId));
     }
+
 }
